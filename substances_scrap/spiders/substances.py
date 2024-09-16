c5c0b9ec1776e4b6f0d99ab06ac5301eb65c4ed7
AndrewDRichter <andrewrichter1@hotmail.com>
2024-09-16 16:31:55 -0400

    m

--- substances_scrap/spiders/substances.py
+++ substances_scrap/spiders/substances.py
@@ -1,10 +1,31 @@
-import scrapy
+from typing import Iterable
+from scrapy import Spider, Request
 
 
-class SubstancesSpider(scrapy.Spider):
+class SubstancesSpider(Spider):
     name = "substances"
-    allowed_domains = ["ilv.ifa.dguv.de"]
-    start_urls = ["https://ilv.ifa.dguv.de/substances"]
+    # allowed_domains = ["ilv.ifa.dguv.de"]
+    # start_urls = ["https://ilv.ifa.dguv.de/substances"]
+
+    def start_requests(self):
+        url = "https://ilv-api.ifa.dguv.de/api/substance"
+        # payload = "searchValue=&pageNr=0&pageSize=24"
+        payload = ""
+        headers = { "Content-Type": "application/json", "Referer": "https://ilv.ifa.dguv.de/"}
+        yield Request(url=url, method="GET", body=payload, headers=headers, callback=self.parse)
+        # yield Request(url=url, method="GET", callback=self.parse)
 
     def parse(self, response):
-        pass
+        data = response.json()
+        print(".......")
+        print(data)
+        print(".......")
+        # for substance in data["content"]:
+        #     id = substance["id"]
+        #     name = substance["name"]
+        #     cas_number = substance["casNrs"]
+        #     yield {
+        #         "id": id,
+        #         "substance": name,
+        #         "casNum": cas_number,
+        #     }
